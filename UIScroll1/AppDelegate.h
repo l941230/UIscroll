20678bf0cf48e8d8590379548c7105efce3af93d
eddie <fxmac@dadaxiaoxiao.local>
2015-03-20 14:04:28 +0800

    Initial Commit

---
+++ UIScroll1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIScroll1
+//
+//  Created by eddie on 15-3-20.
+//  Copyright (c) 2015年 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIScroll1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIScroll1
+//
+//  Created by eddie on 15-3-20.
+//  Copyright (c) 2015年 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
